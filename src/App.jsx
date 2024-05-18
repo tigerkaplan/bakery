9ce551fafb5572865fca216a95a742294a5758ff
tigerkaplane <husniyeerparun@gmail.com>
2024-05-18 16:29:29 +0100

    router updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,25 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+// import components
+import Hero from './components/Hero';
+import About from './components/About';
+import Menu from './components/Menu';
+// import Team from './components/Team';
+// import Testimonial from './components/Testimonial';
+// import Reservation from './components/Reservation';
+import Footer from './components/Footer';
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {
   return (
-<div>bakery</div>
-  )
-}
+    <div className='h-full bg-pattern bg-repeat max-w-[1800px] mx-auto overflow-hidden'>
+      <Hero />
+      <About />
+      <Menu />
+      {/* <Team />
+      <Testimonial />
+      <Reservation /> */}
+      <Footer />
+      <div className='h-[380px] md:h-[370px]'></div>
+    </div>
+  );
+};
 
-export default App
+export default App;

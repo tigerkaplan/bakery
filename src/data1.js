295920a4e3fe4a6f636ccc63082a91ba711e8cfb
tigerkaplane <husniyeerparun@gmail.com>
2024-06-23 16:42:02 +0100

    cop of data

---
+++ src/data1.js
@@ -0,0 +1,549 @@
+// import icons
+import {
+  FaYoutube,
+  FaFacebookF,
+  FaInstagram,
+  FaPinterestP,
+  FaDiscord,
+} from 'react-icons/fa';
+// import images
+import AboutImg from '../src/assets/img/about/plate.png';
+import ModelWhiteImg from '../src/assets/img/model-white.png';
+import ModelBlackImg from '../src/assets/img/model-black.png';
+import MenuImg1 from '../src/assets/img/menu/1.png';
+import MenuImg2 from '../src/assets/img/menu/2.png';
+import MenuImg3 from '../src/assets/img/menu/3.png';
+import MenuImg4 from '../src/assets/img/menu/4.png';
+import SignatureImg from '../src/assets/img/team/signature.png';
+import ChefImg from '../src/assets/img/team/chef.png';
+import Avatar1 from '../src/assets/img/testimonial/avatar1.png';
+import Avatar2 from '../src/assets/img/testimonial/avatar2.png';
+import Avatar3 from '../src/assets/img/testimonial/avatar3.png';
+
+// -------------------------------------------------------------------------------------
+// filter menu
+import menu_1 from "./assets/menu_1.png";
+import menu_2 from "./assets/menu_2.png";
+import menu_3 from "./assets/menu_3.png";
+import menu_4 from "./assets/menu_4.png";
+import menu_5 from "./assets/menu_5.png";
+import menu_6 from "./assets/menu_6.png";
+import menu_7 from "./assets/menu_7.png";
+import menu_8 from "./assets/menu_8.png";
+
+import food_1 from "./assets/food_1.png";
+import food_2 from "./assets/food_2.png";
+import food_3 from "./assets/food_3.png";
+import food_4 from "./assets/food_4.png";
+import food_5 from "./assets/food_5.png";
+import food_6 from "./assets/food_6.png";
+import food_7 from "./assets/food_7.png";
+import food_8 from "./assets/food_8.png";
+import food_9 from "./assets/food_9.png";
+import food_10 from "./assets/food_10.png";
+import food_11 from "./assets/food_11.png";
+import food_12 from "./assets/food_12.png";
+import food_13 from "./assets/food_13.png";
+import food_14 from "./assets/food_14.png";
+import food_15 from "./assets/food_15.png";
+import food_16 from "./assets/food_16.png";
+import food_17 from "./assets/food_17.png";
+import food_18 from "./assets/food_18.png";
+import food_19 from "./assets/food_19.png";
+import food_20 from "./assets/food_20.png";
+import food_21 from "./assets/food_21.png";
+import food_22 from "./assets/food_22.png";
+import food_23 from "./assets/food_23.png";
+import food_24 from "./assets/food_24.png";
+import food_25 from "./assets/food_25.png";
+import food_26 from "./assets/food_26.png";
+import food_27 from "./assets/food_27.png";
+import food_28 from "./assets/food_28.png";
+import food_29 from "./assets/food_29.png";
+import food_30 from "./assets/food_30.png";
+import food_31 from "./assets/food_31.png";
+import food_32 from "./assets/food_32.png";
+
+
+
+export const menu_list = [
+  {
+    menu_name: "Salad",
+    menu_image: menu_1
+  },
+  {
+    menu_name: "Rolls",
+    menu_image: menu_2
+  },
+  {
+    menu_name: "Deserts",
+    menu_image: menu_3
+  },
+  {
+    menu_name: "Sandwich",
+    menu_image: menu_4
+  },
+  {
+    menu_name: "Cake",
+    menu_image: menu_5
+  },
+  {
+    menu_name: "Pure Veg",
+    menu_image: menu_6
+  },
+  {
+    menu_name: "Pasta",
+    menu_image: menu_7
+  },
+  {
+    menu_name: "Noodles",
+    menu_image: menu_8
+  }
+];
+
+export const food_list = [
+  {
+    _id: "1",
+    name: "Greek salad",
+    image: food_1,
+    price: 12,
+    description:
+      "Food provides essential nutrients for overall health and well-being",
+    category: "Salad"
+  },
+  {
+    _id: "2",
+    name: "Veg salad",
+    image: food_2,
+    price: 18,
+    description:
+      "Food provides essential nutrients for overall health and well-being",
+    category: "Salad"
+  },
+  {
+    _id: "3",
+    name: "Clover Salad",
+    image: food_3,
+    price: 16,
+    description:
+      "Food provides essential nutrients for overall health and well-being",
+    category: "Salad"
+  },
+  {
+    _id: "4",
+    name: "Chicken Salad",
+    image: food_4,
+    price: 24,
+    description:
+      "Food provides essential nutrients for overall health and well-being",
+    category: "Salad"
+  },
+  {
+    _id: "5",
+    name: "Lasagna Rolls",
+    image: food_5,
+    price: 14,
+    description:
+      "Food provides essential nutrients for overall health and well-being",
+    category: "Rolls"
+  },
+  {
+    _id: "6",
+    name: "Peri Peri Rolls",
+    image: food_6,
+    price: 12,
+    description:
+      "Food provides essential nutrients for overall health and well-being",
+    category: "Rolls"
+  },
+  {
+    _id: "7",
+    name: "Chicken Rolls",
+    image: food_7,
+    price: 20,
+    description:
+      "Food provides essential nutrients for overall health and well-being",
+    category: "Rolls"
+  },
+  {
+    _id: "8",
+    name: "Veg Rolls",
+    image: food_8,
+    price: 15,
+    description:
+      "Food provides essential nutrients for overall health and well-being",
+    category: "Rolls"
+  },
+  {
+    _id: "9",
+    name: "Ripple Ice Cream",
+    image: food_9,
+    price: 14,
+    description:
+      "Food provides essential nutrients for overall health and well-being",
+    category: "Deserts"
+  },
+  {
+    _id: "10",
+    name: "Fruit Ice Cream",
+    image: food_10,
+    price: 22,
+    description:
+      "Food provides essential nutrients for overall health and well-being",
+    category: "Deserts"
+  },
+  {
+    _id: "11",
+    name: "Jar Ice Cream",
+    image: food_11,
+    price: 10,
+    description:
+      "Food provides essential nutrients for overall health and well-being",
+    category: "Deserts"
+  },
+  {
+    _id: "12",
+    name: "Vanilla Ice Cream",
+    image: food_12,
+    price: 12,
+    description:
+      "Food provides essential nutrients for overall health and well-being",
+    category: "Deserts"
+  },
+  {
+    _id: "13",
+    name: "Chicken Sandwich",
+    image: food_13,
+    price: 12,
+    description:
+      "Food provides essential nutrients for overall health and well-being",
+    category: "Sandwich"
+  },
+  {
+    _id: "14",
+    name: "Vegan Sandwich",
+    image: food_14,
+    price: 18,
+    description:
+      "Food provides essential nutrients for overall health and well-being",
+    category: "Sandwich"
+  },
+  {
+    _id: "15",
+    name: "Grilled Sandwich",
+    image: food_15,
+    price: 16,
+    description:
+      "Food provides essential nutrients for overall health and well-being",
+    category: "Sandwich"
+  },
+  {
+    _id: "16",
+    name: "Bread Sandwich",
+    image: food_16,
+    price: 24,
+    description:
+      "Food provides essential nutrients for overall health and well-being",
+    category: "Sandwich"
+  },
+  {
+    _id: "17",
+    name: "Cup Cake",
+    image: food_17,
+    price: 14,
+    description:
+      "Food provides essential nutrients for overall health and well-being",
+    category: "Cake"
+  },
+  {
+    _id: "18",
+    name: "Vegan Cake",
+    image: food_18,
+    price: 12,
+    description:
+      "Food provides essential nutrients for overall health and well-being",
+    category: "Cake"
+  },
+  {
+    _id: "19",
+    name: "Butterscotch Cake",
+    image: food_19,
+    price: 20,
+    description:
+      "Food provides essential nutrients for overall health and well-being",
+    category: "Cake"
+  },
+  {
+    _id: "20",
+    name: "Sliced Cake",
+    image: food_20,
+    price: 15,
+    description:
+      "Food provides essential nutrients for overall health and well-being",
+    category: "Cake"
+  },
+  {
+    _id: "21",
+    name: "Garlic Mushroom ",
+    image: food_21,
+    price: 14,
+    description:
+      "Food provides essential nutrients for overall health and well-being",
+    category: "Pure Veg"
+  },
+  {
+    _id: "22",
+    name: "Fried Cauliflower",
+    image: food_22,
+    price: 22,
+    description:
+      "Food provides essential nutrients for overall health and well-being",
+    category: "Pure Veg"
+  },
+  {
+    _id: "23",
+    name: "Mix Veg Pulao",
+    image: food_23,
+    price: 10,
+    description:
+      "Food provides essential nutrients for overall health and well-being",
+    category: "Pure Veg"
+  },
+  {
+    _id: "24",
+    name: "Rice Zucchini",
+    image: food_24,
+    price: 12,
+    description:
+      "Food provides essential nutrients for overall health and well-being",
+    category: "Pure Veg"
+  },
+  {
+    _id: "25",
+    name: "Cheese Pasta",
+    image: food_25,
+    price: 12,
+    description:
+      "Food provides essential nutrients for overall health and well-being",
+    category: "Pasta"
+  },
+  {
+    _id: "26",
+    name: "Tomato Pasta",
+    image: food_26,
+    price: 18,
+    description:
+      "Food provides essential nutrients for overall health and well-being",
+    category: "Pasta"
+  },
+  {
+    _id: "27",
+    name: "Creamy Pasta",
+    image: food_27,
+    price: 16,
+    description:
+      "Food provides essential nutrients for overall health and well-being",
+    category: "Pasta"
+  },
+  {
+    _id: "28",
+    name: "Chicken Pasta",
+    image: food_28,
+    price: 24,
+    description:
+      "Food provides essential nutrients for overall health and well-being",
+    category: "Pasta"
+  },
+  {
+    _id: "29",
+    name: "Buttter Noodles",
+    image: food_29,
+    price: 14,
+    description:
+      "Food provides essential nutrients for overall health and well-being",
+    category: "Noodles"
+  },
+  {
+    _id: "30",
+    name: "Veg Noodles",
+    image: food_30,
+    price: 12,
+    description:
+      "Food provides essential nutrients for overall health and well-being",
+    category: "Noodles"
+  },
+  {
+    _id: "31",
+    name: "Somen Noodles",
+    image: food_31,
+    price: 20,
+    description:
+      "Food provides essential nutrients for overall health and well-being",
+    category: "Noodles"
+  },
+  {
+    _id: "32",
+    name: "Cooked Noodles",
+    image: food_32,
+    price: 15,
+    description:
+      "Food provides essential nutrients for overall health and well-being",
+    category: "Noodles"
+  }
+];
+
+
+// -------------------------------------- ends filter menu ----------------------------------------
+export const navData = [
+  { href: '/', name: 'home' },
+  { href: '/', name: 'about' },
+  { href: '/', name: 'menu' },
+  { href: '/', name: 'team' },
+  { href: '/', name: 'testimonials' },
+  { href: '/', name: 'book a table' },
+  { href: '/', name: 'contact' },
+];
+
+export const heroData = {
+  pretitle: 'Nothing brings together like',
+  title: 'Patisserie 4 you',
+  subtitle:
+    'Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. ',
+  btnText: 'Find out more',
+};
+
+export const socialData = [
+  { href: '/', icon: <FaYoutube /> },
+  { href: '/', icon: <FaFacebookF /> },
+  { href: '/', icon: <FaInstagram /> },
+  { href: '/', icon: <FaPinterestP /> },
+  { href: '/', icon: <FaDiscord /> },
+];
+
+export const aboutData = {
+  pretitle: 'our story',
+  title: 'who we are',
+  subtitle:
+    'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Amet consequatur, quasi minima molestiae ex exercitationem culpa quisquam architecto quaerat, iusto dolores labore, sapiente magni rem commodi aperiam ad dolorem neque ducimus. Placeat vel non quod quis pariatur a aperiam, aliquam adipisci voluptatum voluptatem sit cupiditate dolore natus beatae earum omnis.',
+  btnText: 'find out more',
+  image: AboutImg,
+};
+
+export const menuData = {
+  title: 'delicious flavour of autumn',
+  subtitle: 'view all menu for tasty meal today',
+  modelImg: ModelWhiteImg,
+  btnText: 'view complete menu',
+  menuItems: [
+    {
+      image: MenuImg1,
+      name: 'Chocolate Cake',
+      price: '$7.99',
+      description: 'Class aptent taciti ciosqu litora torquent per',
+    },
+    {
+      image: MenuImg2,
+      name: 'Veggie Burger',
+      price: '$9.49',
+      description: 'Class aptent taciti ciosqu litora torquent per',
+    },
+    {
+      image: MenuImg3,
+      name: 'King Burger',
+      price: '$8.50',
+      description: 'Class aptent taciti ciosqu litora torquent per',
+    },
+    {
+      image: MenuImg4,
+      name: 'Mexican Burger',
+      price: '$9.99',
+      description: 'Class aptent taciti ciosqu litora torquent per',
+    },
+  ],
+};
+
+export const teamData = {
+  pretitle: 'our team',
+  title: 'meet our chef',
+  sub1: ' Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis adipisci repudiandae enim ratione corrupti voluptatum suscipit distinctio dolor.',
+  sub2: 'Sequi exercitationem quae deserunt reiciendis nesciunt perferendis atque quisquam, odit facere! Minima esse reiciendis, magnam fugiat totam maxime consequatur perspiciatis voluptas consequuntur.',
+  name: 'sara peter',
+  occupation: 'executive chef',
+  signatureImg: SignatureImg,
+  chefImg: ChefImg,
+};
+
+export const testimonialData = {
+  title: "what client's say ",
+  subtitle: '1500+ statisfied clients',
+  modelImg: ModelWhiteImg,
+  slider: [
+    {
+      message:
+        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio quas ipsum eius voluptatibus. Quod ipsum ullam id facere a beatae incidunt eaque, veritatis architecto cum perferendis debitis tempora.',
+      image: Avatar1,
+      name: 'Rick Thompson',
+      occupation: 'CEO of Delightful',
+    },
+    {
+      message:
+        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio quas ipsum eius voluptatibus. Quod ipsum ullam id facere a beatae incidunt eaque, veritatis architecto cum perferendis debitis tempora.',
+      image: Avatar2,
+      name: 'John Doe',
+      occupation: 'CEO of Delightful',
+    },
+    {
+      message:
+        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio quas ipsum eius voluptatibus. Quod ipsum ullam id facere a beatae incidunt eaque, veritatis architecto cum perferendis debitis tempora.',
+      image: Avatar3,
+      name: 'Henry A.',
+      occupation: 'CEO of Delightful',
+    },
+  ],
+};
+
+export const reservationData = {
+  title: 'booking form',
+  subtitle:
+    'Call (800) 123-4567 from 5AM - 11PM daily, or book online with OpenTable.Reservations required for parties of 6 or more.',
+  modelImg: ModelBlackImg,
+  btnText: 'make a reservation',
+};
+
+export const newsletterData = {
+  title: 'join our newsletter',
+  subtitle: 'Get latest news & updates in your inbox.',
+  placeholder: 'Subscribe our delicious dishes',
+  btnText: 'subscribe now',
+};
+
+export const footerData = {
+  contact: {
+    title: 'contact location',
+    address: '3784 patterson road, #8 new york, CA 69000',
+    phone: '(201)256-3689',
+  },
+  hours: {
+    title: 'working hours',
+    program: [
+      {
+        days: 'monday - friday',
+        hours: '09:00 AM - 10:00 PM',
+      },
+      {
+        days: 'saturday - sunday',
+        hours: '09:00 AM - 11:00 PM',
+      },
+    ],
+  },
+  social: {
+    title: 'social network',
+    icons: [
+      { href: '/', icon: <FaYoutube /> },
+      { href: '/', icon: <FaFacebookF /> },
+      { href: '/', icon: <FaInstagram /> },
+      { href: '/', icon: <FaPinterestP /> },
+      { href: '/', icon: <FaDiscord /> },
+    ],
+  },
+};
